ba4e1ad63a6ef21820b284d4fd700513f252d04c
lacalice <lcalice@ensc.fr>
2019-11-28 16:10:51 +0100

    modification du cercle de corrélation

--- ACP.R
+++ ACP.R
@@ -1,4 +1,4 @@
-path<-("activation2020.Rdata")
+path<-("F:/2A/modelisation_stat/projetR/activation2020.Rdata")
 donnees<-readRDS(path)
 head(donnees)
 dim(donnees)
@@ -12,8 +12,16 @@ if(!require(PCAmixdata)) {
 }
 
 donne<-c(age, volume, frontal_1R,angular_2R,occi_1R,rolandic_1R,hippo_1R,tempo_4R,frontal_1L,angular_2L,occi_1L,rolan_1L,hippo_1L,tempo_4L)
+#res.pca<-PCAmix(X.quali = donnees[,c("Preference_Manuelle","Sexe")],
+#               X.quanti = donnees[,c("Age","Volume_Cerebral","Index_Lateralisation_Hemispherique","Prod_G_Frontal_Inf_Tri_1_R","Prod_G_Angular_2_R","Prod_G_Occipital_Lat_1_R","Prod_G_Rolandic_Oper_1_R","Prod_G_Hippocampus_1_R","Prod_S_Sup_Temporal_4_R","Prod_G_Frontal_Inf_Tri_1_L","Prod_G_Angular_2_L","Prod_G_Occipital_Lat_1_L","Prod_G_Rolandic_Oper_1_L","Prod_G_Hippocampus_1_L","Prod_S_Sup_Temporal_4_L")])
+
 res.pca<-PCAmix(X.quali = donnees[,c("Preference_Manuelle","Sexe")],
-               X.quanti = donnees[,c("Age","Volume_Cerebral","Index_Lateralisation_Hemispherique","Prod_G_Frontal_Inf_Tri_1_R","Prod_G_Angular_2_R","Prod_G_Occipital_Lat_1_R","Prod_G_Rolandic_Oper_1_R","Prod_G_Hippocampus_1_R","Prod_S_Sup_Temporal_4_R","Prod_G_Frontal_Inf_Tri_1_L","Prod_G_Angular_2_L","Prod_G_Occipital_Lat_1_L","Prod_G_Rolandic_Oper_1_L","Prod_G_Hippocampus_1_L","Prod_S_Sup_Temporal_4_L")])
+                X.quanti = donnees[,c("Prod_G_Frontal_Inf_Tri_1_R",
+                                      "Prod_G_Occipital_Lat_1_R","Prod_G_Rolandic_Oper_1_R",
+                                      "Prod_G_Hippocampus_1_R","Prod_S_Sup_Temporal_4_R",
+                                      "Prod_G_Frontal_Inf_Tri_1_L","Prod_G_Occipital_Lat_1_L",
+                                      "Prod_G_Rolandic_Oper_1_L","Prod_G_Hippocampus_1_L",
+                                      "Prod_S_Sup_Temporal_4_L")])
 
 
 #Choix du nombre d'axes à retenir
@@ -21,16 +29,18 @@ par(mfrow=c(1,1))
 round(res.pca$eig,digit=2) #affiche les valeurs propres et les pourcentages de variances expliquées par chaque axe
 #on prend les 6 premiers axes car après en dessous de 1
 
+
 #infos sur les dimensions
 barplot(res.pca$eig[,1],main="Eigenvalues",names.arg=1:nrow(res.pca$eig))
 abline(h=1,col=2,lwd=2)
 plot(res.pca)
 
-#graphiques des individus
+#graphiques 
 par(mfrow=c(1,2))
-#plot(res,axes=c(1,2),choice="ind",label=FALSE)
+plot(res,axes=c(1,2),choice="ind",label=FALSE) #graphique des individus
 plot(res.pca,axes=c(1,2),choice="cor") #cercle de corrélations
 plot(res.pca,axes=c(1,2),choice="sqload")
+#on remarque que Prod_G_ROlandic_Oper_1_R et ****_L sont corrélés
 res.pca$ind
 round(res.pca$ind$cos2,digit=3)
 res.pca$quanti
